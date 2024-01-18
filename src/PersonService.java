ffc56d76632f19572fc2698148cf01d9dc46a68f
medertynychbekov <meder.tynychbekov@gmail.com>
2024-01-18 20:08:40 +0300

    Initial commit

---
+++ src/PersonService.java
@@ -0,0 +1,43 @@
+public class PersonService {
+    private Person[] people = new Person[10];
+
+    /**
+     * Параметрден келе жаткан person объекти
+     * PersonService класстын ичиндеги people массивге салыныз
+     */
+    public Person create(Person person) {
+        return null;
+    }
+
+    /**
+     * person объекти IDси менен массивден таап чыгыныз.
+     * Параметрден келе жаткан newPerson объектин маанилери менен алмаштырып,
+     * кайрадан массивге салып коюнуз.
+     */
+    public Person update(int id, Person newPerson) {
+
+        return null;
+    }
+
+    /**
+     * Массивдин ичинен person объекти ID менен таап, кайтарып бериши керек
+     */
+    public Person findById(int id) {
+        return null;
+    }
+
+    /**
+     * people массивин кайтарып бериши керек.
+     */
+    public Person[] findAll() {
+        return null;
+    }
+
+    /**
+     * Массивдин ичинен person объекти ID менен таап,
+     * массивден очуруп салыныз(nullга барабарлап коюнуз).
+     */
+    public void deleteById(int id) {
+
+    }
+}
